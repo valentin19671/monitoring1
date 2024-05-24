6307763436d316ad342b6e70c56201f8da9d6bed
valentin19671 <150732112+valentin19671@users.noreply.github.com>
2024-05-24 13:30:36 +0300

    1

---
+++ main.js
@@ -0,0 +1,56 @@
+/*let navBlockArr=document.querySelectorAll('.left-nav-block');
+
+let p1HoverArr=document.querySelectorAll('.p1-hover');
+
+navBlockArr[0].addEventListener('mouseover',()=>{
+    p1HoverArr[0].style.opacity='1';
+})
+
+navBlockArr[0].addEventListener('mouseout',()=>{
+    p1HoverArr[0].style.opacity='0';
+})
+
+
+navBlockArr[1].addEventListener('mouseover',()=>{
+    p1HoverArr[1].style.opacity='1';
+})
+
+navBlockArr[1].addEventListener('mouseout',()=>{
+    p1HoverArr[1].style.opacity='0';
+})
+
+
+navBlockArr[2].addEventListener('mouseover',()=>{
+    p1HoverArr[2].style.opacity='1';
+})
+
+navBlockArr[2].addEventListener('mouseout',()=>{
+    p1HoverArr[2].style.opacity='0';
+})
+
+
+navBlockArr[3].addEventListener('mouseover',()=>{
+    p1HoverArr[3].style.opacity='1';
+})
+
+navBlockArr[3].addEventListener('mouseout',()=>{
+    p1HoverArr[3].style.opacity='0';
+})
+
+
+navBlockArr[4].addEventListener('mouseover',()=>{
+    p1HoverArr[4].style.opacity='1';
+})
+
+navBlockArr[4].addEventListener('mouseout',()=>{
+    p1HoverArr[4].style.opacity='0';
+})
+
+
+navBlockArr[5].addEventListener('mouseover',()=>{
+    p1HoverArr[5].style.opacity='1';
+})
+
+navBlockArr[5].addEventListener('mouseout',()=>{
+    p1HoverArr[5].style.opacity='0';
+})*/
